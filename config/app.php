e6eff016ca61bb3fff6013266b621fa14e64c52d
noescom <E934BFdkKtWndW7>
2025-06-10 11:54:13 +0200

    Added comments to app.php

--- config/app.php
+++ config/app.php
@@ -1,7 +1,15 @@
 <?php
+
+	// Base source directory relative to config folder
+	$srcDir = dirname(__DIR__) . '/src';
 	
 	return [
-		'controller_directory'   => __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'Controllers',
-		'aspect_directory'       => __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'Aspects',
+		// Path to controller classes
+		'controller_directory'   => $srcDir . '/Controllers',
+		
+		// Path to aspect-oriented programming files
+		'aspect_directory'       => $srcDir . '/Aspects',
+		
+		// Whether to match routes with trailing slashes
 		'match_trailing_slashes' => false
 	];
