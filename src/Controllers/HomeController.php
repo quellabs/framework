fd757f7e5b763dfab65bba3bdd29d6bcc35b0729
noescom <E934BFdkKtWndW7>
2025-09-04 11:30:45 +0200

    Added origin field to AstSubquery

--- src/Controllers/HomeController.php
+++ src/Controllers/HomeController.php
@@ -17,7 +17,7 @@
 			$rs = $this->em()->executeQuery("
 				range of c is PostEntity
 				range of d is PostEntity via d.title=c.title
-				retrieve (SUM(d.id), d.title)
+				retrieve (SUM(d.id))
 			");
 			
 			// SELECT COALESCE(SUM(d.id), 0) as `SUM(d.id)` FROM `posts` as `c` LEFT JOIN `posts` as `d` ON d.title = c.title
