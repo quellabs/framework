627c57f99be7442aa5b0e724e4cb10b1d0bc867d
noescom <E934BFdkKtWndW7>
2026-03-06 17:30:46 +0100

    typo

--- config/inspector.php
+++ config/inspector.php
@@ -3,9 +3,8 @@
 	return [
 		// True to put the framework in debug mode; this mainly affects caching
 		'enabled' => true,
-		/*
 		'panels' => [
+			\Quellabs\Canvas\Inspector\Panels\QueryPanel::class,
 			\Quellabs\Canvas\Inspector\Panels\WakaPACPanel::class
 		]
-		*/
 	];
