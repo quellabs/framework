c15d5156be506d1c54557d8c3339c5c615947bee
noescom <E934BFdkKtWndW7>
2026-03-17 09:00:17 +0100

    fixes

--- packages/canvas-payments-contracts/src/InitiateResponse.php
+++ packages/canvas-payments-contracts/src/InitiateResponse.php
@@ -9,14 +9,23 @@
 	final readonly class InitiateResponse {
 		
 		/**
-		 * @param string $provider      Identifier of the payment provider that created this session (e.g. 'mollie', 'stripe').
+		 * InitiateResponse constructor
+		 * @param string $provider Identifier of the payment provider that created this session (e.g. 'mollie', 'stripe').
 		 * @param string $transactionId Provider-assigned transaction ID, used for status checks and refunds.
-		 * @param string $redirectUrl   URL to redirect the customer to in order to complete the payment.
+		 * @param string $redirectUrl URL to redirect the customer to in order to complete the payment.
 		 */
 		public function __construct(
 			public string $provider,
 			public string $transactionId,
 			public string $redirectUrl,
 		) {
+		}
+		
+		public function toArray(): array {
+			return [
+				'provider'       => $this->provider,
+				'transaction_id' => $this->transactionId,
+				'redirect_url'   => $this->redirectUrl,
+			];
 		}
 	}
