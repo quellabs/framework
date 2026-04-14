e62bfc0c2dd458366439e8d62c60e1039921bcca
noescom <E934BFdkKtWndW7>
2026-04-14 08:10:12 +0200

    Added sidebar

--- packages/canvas-loom/src/Builder/Column.php
+++ packages/canvas-loom/src/Builder/Column.php
@@ -14,6 +14,20 @@
 		}
 		
 		/**
+		 * Mark this column as a sidebar, rendering a title and hint text
+		 * instead of form fields, separated from the content by a vertical line.
+		 * @param string $title Title shown at the top of the sidebar
+		 * @param string $hint  Optional hint text shown below the title
+		 * @return static
+		 */
+		public function markAsSidebar(string $title, string $hint = ''): static {
+			$this->set('sidebar', true);
+			$this->set('sidebar_title', $title);
+			$this->set('sidebar_hint', $hint);
+			return $this;
+		}
+		
+		/**
 		 * Return node type
 		 * @return string
 		 */
--- packages/canvas-loom/src/Renderer/ColumnRenderer.php
+++ packages/canvas-loom/src/Renderer/ColumnRenderer.php
@@ -8,36 +8,72 @@
 	/**
 	 * Renders a single column within a columns layout.
 	 * Width is injected by the parent ColumnsRenderer as a percentage.
+	 * A column can optionally be marked as a sidebar, rendering a title
+	 * and hint text instead of form fields, separated from the content
+	 * by a vertical line.
+	 *
+	 * CSS classes are defined as protected properties so theme packages
+	 * can extend this renderer and override only the class names.
 	 */
 	class ColumnRenderer extends AbstractRenderer {
 		
 		/** @var string Wrapper div class */
 		protected string $wrapperClass = 'loom-column';
 		
+		/** @var string Sidebar wrapper class */
+		protected string $sidebarClass = 'loom-column-sidebar';
+		
+		/** @var string Sidebar title class */
+		protected string $sidebarTitleClass = 'loom-column-sidebar-title';
+		
+		/** @var string Sidebar hint class */
+		protected string $sidebarHintClass = 'loom-column-sidebar-hint';
+		
 		/**
 		 * Render the column
-		 * @param array $properties
-		 * @param string $children
-		 * @param array|null $parent
-		 * @param int $index
+		 * @param array      $properties Node properties from the JSON definition
+		 * @param string     $children   Already-rendered HTML of all child nodes
+		 * @param array|null $parent     Parent node
+		 * @param int        $index      Index of this node within its parent
 		 * @return RenderResult
 		 */
 		public function render(array $properties, string $children, ?array $parent = null, int $index = 0): RenderResult {
-			$class = $properties['class'] ?? $this->wrapperClass;
-			$widths = $parent['properties']['widths'] ?? [];
+			$width   = $properties['width'] ?? null;
+			$sidebar = !empty($properties['sidebar']);
 			
-			// Determine width from parent widths array, fall back to equal distribution
-			if (isset($widths[$index])) {
-				$styleAttr = " style=\"flex: 0 0 {$widths[$index]}%; min-width: 0;\"";
+			// Apply width as inline flex style if provided
+			$styleAttr = $width !== null
+				? " style=\"flex: 0 0 {$width}%; min-width: 0;\""
+				: " style=\"flex: 1; min-width: 0;\"";
+			
+			if ($sidebar) {
+				$title    = $properties['sidebar_title'] ?? '';
+				$hint     = $properties['sidebar_hint']  ?? '';
+				$class    = $properties['class']         ?? $this->sidebarClass;
+				
+				$titleHtml = $title
+					? "<p class=\"{$this->sidebarTitleClass}\">{$title}</p>"
+					: '';
+				
+				$hintHtml = $hint
+					? "<p class=\"{$this->sidebarHintClass}\">{$hint}</p>"
+					: '';
+				
+				$html = <<<HTML
+            <div class="{$class}"{$styleAttr}>
+                {$titleHtml}
+                {$hintHtml}
+            </div>
+            HTML;
 			} else {
-				$styleAttr = " style=\"flex: 1; min-width: 0;\"";
+				$class = $properties['class'] ?? $this->wrapperClass;
+				
+				$html = <<<HTML
+            <div class="{$class}"{$styleAttr}>
+                {$children}
+            </div>
+            HTML;
 			}
-			
-			$html = <<<HTML
-    <div class="{$class}"{$styleAttr}>
-        {$children}
-    </div>
-    HTML;
 			
 			return new RenderResult($html);
 		}
