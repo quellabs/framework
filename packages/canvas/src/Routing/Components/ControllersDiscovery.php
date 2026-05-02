8cd59b222ddcec6df0c05fb7c53650714d03af80
noescom <E934BFdkKtWndW7>
2026-05-02 14:44:03 +0200

    phpstan fixes

--- packages/canvas/src/Routing/Components/ControllersDiscovery.php
+++ packages/canvas/src/Routing/Components/ControllersDiscovery.php
@@ -45,7 +45,7 @@
 			// Collect class names advertised by packages
 			$packageClasses = array_filter(
 				$discover->getFamilyValues('canvas', 'controller'),
-				fn($value) => is_string($value) && class_exists($value)
+				fn($value) => class_exists($value)
 			);
 			
 			// Return a flat list of fully qualified controller class names
