0d98808dff33cf5e5089fe7bd22c6316cd8a2005
noescom <E934BFdkKtWndW7>
2026-05-03 16:22:35 +0200

    Removed QueryBuilder repetition code

--- src/Controllers/BlogController.php
+++ src/Controllers/BlogController.php
@@ -30,8 +30,6 @@
 		public function show(int $id): Response {
 			$post = $this->em()->find(PostEntity::class, $id);
 			
-			echo $post->user->getId();
-			
 			if (!$post) {
 				return $this->notFound('Post does not exist.');
 			}
