cb00442fbc5c3df9256ab8e4f4a2732a657ceb9a
noescom <E934BFdkKtWndW7>
2026-05-16 08:06:31 +0200

    removed test code

--- src/Controllers/BlogController.php
+++ src/Controllers/BlogController.php
@@ -15,16 +15,6 @@
 		 * @return Response
 		 */
 		public function index(): Response {
-			
-			$x = $this->em()->executeQuery("
-				range of y is json_source('f:\\test.json', '$.rows')
-				retrieve(count(y.id where y.id > 0))
-			");
-			
-			foreach($x as $row) {
-				print_r($row);
-			}
-			
 			$posts = $this->em()->findBy(PostEntity::class, ['published' => true]);
 			
 			return $this->render("blog/index.tpl", [
