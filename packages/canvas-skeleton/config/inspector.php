ae14cd9852fd748cf826ab8cc244beaf09547e37
noescom <E934BFdkKtWndW7>
2026-06-01 07:42:29 +0200

    Extended default inspector

--- packages/canvas-skeleton/config/inspector.php
+++ packages/canvas-skeleton/config/inspector.php
@@ -1,6 +1,12 @@
 <?php
 	
 	return [
-		// True to put the framework in debug mode; this mainly affects caching
+		// Show the debug panel
 		'enabled' => true,
+		
+		// Optional panels that are loaded besides the built in Request panel
+		'panels' => [
+			\Quellabs\Canvas\Inspector\Panels\QueryPanel::class,
+			\Quellabs\Canvas\Inspector\Panels\WakaPACPanel::class
+		]
 	];
