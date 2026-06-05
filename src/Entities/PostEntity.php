9447c2ebfa3a3e5c5dfcfd65b3e14668a3031dd9
noescom <E934BFdkKtWndW7>
2026-06-05 13:50:04 +0200

    typo

--- src/Entities/PostEntity.php
+++ src/Entities/PostEntity.php
@@ -72,6 +72,11 @@
 		protected ?int $userId = null;
 
 		/**
+		* @Orm\InverseOf(targetEntity="BananaEntity", relation="rel", fetch="LAZY")
+		*/
+		protected ?BananaEntity $banana;
+
+		/**
 		 * Get id
 		 * @return int
 		 */
@@ -186,4 +191,31 @@
 			$this->testJSON = $testJSON;
 			return $this;
 		}
-	}
+	
+		/**
+		 * Gets the banana relationship
+		 * @return ?BananaEntity
+		 */
+		public function getBanana(): ?BananaEntity {
+				return $this->banana;
+		}
+
+
+		/**
+		 * Sets the banana relationship
+		 * @param ?BananaEntity $banana The related entity
+		 * @return $this
+		 */
+		public function setBanana(?BananaEntity $banana): self {
+				// Prevent redundant updates
+				if ($this->banana === $banana) {
+						return $this;
+				}
+
+				// Set new property
+				$this->banana = $banana;
+
+				return $this;
+		}
+
+}
